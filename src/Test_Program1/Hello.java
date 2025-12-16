5b6209827bde07f8b15f2e0ef27b874cc0bf90bb
nidhigup88 <nidhigup.88@gmail.com>
2025-12-16 17:45:30 +0530

    newly created java class

---
+++ src/Test_Program1/Hello.java
@@ -0,0 +1,8 @@
+package Test_Program1;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Test_Program1.Hello!");
+        System.out.println("Test_Program1.Hello nidhi!");
+    }
+}
---
+++ src/Test_Program1/Hello_Mishi.java
@@ -0,0 +1,7 @@
+package Test_Program1;
+
+public class Hello_Mishi {
+    static void main() {
+        System.out.println("Hello Mishi");
+    }
+}
